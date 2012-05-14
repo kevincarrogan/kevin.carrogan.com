0f491e8ffb989d006e7c03587b16fa4f7866cf61
Kevin Morgan <kevin.morgan@hogarthww.com>
2012-05-15 00:22:20 +0100

    Stopping displaying map when on a touch device.

--- static/js/script.js
+++ static/js/script.js
@@ -1,40 +1,42 @@
 /*global _, google, Hogan*/
 $(function () {
     (function () {
-        var locations = []
-          , mapOptions
-          , map
-          , geocoder
-          , bounds;
+        if (!Modernizr.touch) {
+            var locations = []
+              , mapOptions
+              , map
+              , geocoder
+              , bounds;
 
-        geocoder = new google.maps.Geocoder();
-        bounds = new google.maps.LatLngBounds();
+            geocoder = new google.maps.Geocoder();
+            bounds = new google.maps.LatLngBounds();
 
-        mapOptions = {
-            center: bounds.getCenter(),
-            zoom: 8,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map($('#personal-map').get(0), mapOptions);
+            mapOptions = {
+                center: bounds.getCenter(),
+                zoom: 8,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            map = new google.maps.Map($('#personal-map').get(0), mapOptions);
 
-        _.each($('a[data-map]'), function (elem) {
-            var location = $(elem).data('map')
-              , marker;
-            geocoder.geocode(
-                {
-                    address: location
-                },
-                function (result) {
-                    marker = new google.maps.Marker({
-                        map: map,
-                        position: result[0].geometry.location
-                    });
-                    map.setCenter(result[0].geometry.location);
-                    bounds.extend(result[0].geometry.location);
-                }
-            );
-        });
-        map.fitBounds(bounds);
+            _.each($('a[data-map]'), function (elem) {
+                var location = $(elem).data('map')
+                  , marker;
+                geocoder.geocode(
+                    {
+                        address: location
+                    },
+                    function (result) {
+                        marker = new google.maps.Marker({
+                            map: map,
+                            position: result[0].geometry.location
+                        });
+                        map.setCenter(result[0].geometry.location);
+                        bounds.extend(result[0].geometry.location);
+                    }
+                );
+            });
+            map.fitBounds(bounds);
+        }
     }());
 
     (function () {

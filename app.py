f39ab6edca8b5432ca1f5e4b04fbcf04df095fdc
Kevin Morgan <kevin.morgan@hogarthww.com>
2012-05-15 17:59:22 +0100

    Revert "Revert "Stopped using islice in itertools.""
    This reverts commit 9830649c5f3f0a133069cb9a94cda118317d3b65.

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pystache
 import feedparser
 import json
 
-from itertools import islice
-
 from pystache.loader import Loader
 
 from flask import Flask
@@ -29,10 +27,13 @@ def index():
     for name, url in feeds:
         feed = feedparser.parse(url)
         current = feed.entries[0]
+        items = []
+        for i in range(5):
+            items.append(feed.entries[i].title)
         feed_results[name] = {
             'title': current.title,
             'link': current.link,
-            'items': json.dumps([item.title for item in islice(feed.entries, 5)])
+            'items': json.dumps(items)
         }
 
     return pystache.render(

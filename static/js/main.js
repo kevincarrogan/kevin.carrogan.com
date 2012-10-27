91d3012ef1bc86ae5c2ea979ebaf4786b5d7c575
Kevin Morgan <kevinm@potatolondon.com>
2012-10-27 18:38:10 +0100

    Adding all background images for films.

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,15 @@
 (function ($) {
-  var $body = $('body'),
-      originalImage = $body.css('background-image');
+  var $html = $('html'),
+      originalImage = $html.css('background-image');
   $('[data-large-image]').each(function (index, el) {
     var $el = $(el);
 
     $el.hover(
       function () {
-        $body.css('background-image', 'url(' + $el.data('large-image') + ')');
+        $html.css('background-image', 'url(' + $el.data('large-image') + ')');
       },
       function () {
-        $body.css('background-image', originalImage);
+        $html.css('background-image', originalImage);
       }
     );
   });

fcb92c22135827ae5772eb46871e128df6ee3f5d
Kevin Morgan <kevinm@potatolondon.com>
2012-10-27 18:46:49 +0100

    Preloading images.

--- static/js/main.js
+++ static/js/main.js
@@ -2,11 +2,15 @@
   var $html = $('html'),
       originalImage = $html.css('background-image');
   $('[data-large-image]').each(function (index, el) {
-    var $el = $(el);
+    var $el = $(el),
+        imageUrl = $el.data('large-image'),
+        img = new Image();
+
+    img.src = imageUrl;
 
     $el.hover(
       function () {
-        $html.css('background-image', 'url(' + $el.data('large-image') + ')');
+        $html.css('background-image', 'url(' + imageUrl + ')');
       },
       function () {
         $html.css('background-image', originalImage);

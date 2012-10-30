5fd6c164af37dbe76c0b307179775fed366e4db0
Kevin Morgan <kevinm@potatolondon.com>
2012-10-30 14:35:13 +0000

    Adding map on personal information page.

--- static/js/main.js
+++ static/js/main.js
@@ -18,3 +18,58 @@
     );
   });
 })(window.jQuery);
+
+(function ($) {
+  var $mapContainer = $('.personal-map'),
+      location = [],
+      mapOptions,
+      map,
+      geocoder,
+      bounds;
+
+  geocoder = new google.maps.Geocoder();
+  bounds = new google.maps.LatLngBounds();
+
+  mapOptions = {
+    center: bounds.getCenter(),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map($mapContainer.get(0), mapOptions);
+
+  $('a[data-map]').each(function (i, elem) {
+    var location = $(elem).data('map'),
+        result;
+    geocoder.geocode(
+      {
+        address: location
+      },
+      function (result) {
+        new google.maps.Marker({
+          map: map,
+          position: result[0].geometry.location
+        });
+        bounds.extend(result[0].geometry.location);
+        $(elem).hover(
+          function (evt) {
+            map.panTo(result[0].geometry.location);
+          },
+          function (evt) {
+            map.panTo(bounds.getCenter());
+          }
+        );
+        map.setCenter(bounds.getCenter());
+        map.fitBounds(bounds);
+      }
+    );
+  });
+
+  $('.personal-information').hover(
+    function (evt) {
+      $mapContainer.css('opacity', 1);
+    },
+    function (evt) {
+      $mapContainer.css('opacity', 0);
+    }
+  );
+})(window.jQuery);

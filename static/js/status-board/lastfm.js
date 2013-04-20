d4842e2e2882d470a696b155806a06e013334970
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-04-20 13:58:49 +0100

    Fixing so that it actually works on the status board.

--- static/js/status-board/lastfm.js
+++ static/js/status-board/lastfm.js
@@ -1,33 +1,34 @@
 (function () {
+
     var src = new EventSource('.'),
-        $trackInformation = document.querySelector('.track-information'),
-        $newTrackInformation = document.querySelector('.new-track-information');
+        $trackInformation = $('.track-information'),
+        $newTrackInformation = $('.new-track-information');
 
     src.addEventListener('lastfm', function (evt) {
         var trackData = JSON.parse(evt.data);
 
         // Add new content
-        $newTrackInformation.querySelector('.track').innerText = trackData['title'];
-        $newTrackInformation.querySelector('.artist').innerText = trackData['artist'];
+        $newTrackInformation.find('.track').text(trackData['title']);
+        $newTrackInformation.find('.artist').text(trackData['artist']);
 
         // Transition in the new track
-        $newTrackInformation.classList.add('track-in');
+        $newTrackInformation.addClass('track-in');
 
         // Transition out the old track
-        $trackInformation.classList.add('track-out');
+        $trackInformation.addClass('track-out');
 
-        $trackInformation.addEventListener('webkitTransitionEnd', function (evt) {
-            $trackInformation.classList.remove('track-transition');
-            $trackInformation.classList.remove('track-out');
-            $trackInformation.querySelector('.track').innerText = trackData['title'];
-            $trackInformation.querySelector('.artist').innerText = trackData['artist'];
-            setTimeout(function () { $trackInformation.classList.add('track-transition'); }, 0);
+        $trackInformation.get(0).addEventListener('webkitTransitionEnd', function (evt) {
+            $trackInformation.removeClass('track-transition');
+            $trackInformation.removeClass('track-out');
+            $trackInformation.find('.track').text(trackData['title']);
+            $trackInformation.find('.artist').text(trackData['artist']);
+            setTimeout(function () { $trackInformation.addClass('track-transition'); }, 0);
         }, true);
 
-        $newTrackInformation.addEventListener('webkitTransitionEnd', function (evt) {
-            $newTrackInformation.classList.remove('track-transition');
-            $newTrackInformation.classList.remove('track-in');
-            setTimeout(function () { $newTrackInformation.classList.add('track-transition'); }, 0);
+        $newTrackInformation.get(0).addEventListener('webkitTransitionEnd', function (evt) {
+            $newTrackInformation.removeClass('track-transition');
+            $newTrackInformation.removeClass('track-in');
+            setTimeout(function () { $newTrackInformation.addClass('track-transition'); }, 0);
         }, true);
 
     });

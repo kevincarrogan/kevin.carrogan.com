a725232072b1160a5fe341bd5a0fb7c361611390
Kevin Carrogan <kevin.carrogan@gmail.com>
2014-08-30 14:28:43 +0100

    Ignoring any failures when trying to communicate with last fm.

--- app.py
+++ app.py
@@ -33,27 +33,32 @@ def index():
     loader = Loader()
     template = loader.load_name('index')
 
-    lastfm_result = cache.get('lastfm_result')
-    if not lastfm_result:
-        lastfm_client = LastfmClient(lastfm_key, lastfm_secret)
-        recent_tracks = lastfm_client.user.get_recent_tracks('kevbear')['track']
-        recent_track = recent_tracks[0]
-        artist_name = recent_track['artist']['#text']
-        track_name = recent_track['name']
-        mbid = recent_track['mbid']
-        track_info = lastfm_client.track.get_info(
-            artist_name,
-            track_name,
-            mbid,
-        )
-        lastfm_result = {
-            'track': track_info['name'],
-            'duration': milli_seconds_to_duration(int(track_info['duration'])),
-            'album': track_info['album']['title'],
-            'artist': track_info['artist']['name'],
-            'url': track_info['url'],
-        }
-        cache.set('lastfm_result', lastfm_result, timeout=120)
+    lastfm_result = None
+
+    try:
+        lastfm_result = cache.get('lastfm_result')
+        if not lastfm_result:
+            lastfm_client = LastfmClient(lastfm_key, lastfm_secret)
+            recent_tracks = lastfm_client.user.get_recent_tracks('kevbear')['track']
+            recent_track = recent_tracks[0]
+            artist_name = recent_track['artist']['#text']
+            track_name = recent_track['name']
+            mbid = recent_track['mbid']
+            track_info = lastfm_client.track.get_info(
+                artist_name,
+                track_name,
+                mbid,
+            )
+            lastfm_result = {
+                'track': track_info['name'],
+                'duration': milli_seconds_to_duration(int(track_info['duration'])),
+                'album': track_info['album']['title'],
+                'artist': track_info['artist']['name'],
+                'url': track_info['url'],
+            }
+            cache.set('lastfm_result', lastfm_result, timeout=120)
+    except:
+        pass
 
     ctx = {
         'lastfm_result': lastfm_result,

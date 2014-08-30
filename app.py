94e8bccc5074de1a9049b1705a1bc61f2f9dc653
Kevin Carrogan <kevin.carrogan@gmail.com>
2014-08-30 14:25:09 +0100

    Adding some basic caching to not have to retrieve last fm results each time.

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ import gevent.monkey
 from gevent.pywsgi import WSGIServer
 gevent.monkey.patch_all()
 
+from werkzeug.contrib.cache import SimpleCache
+
 from lastfmclient import LastfmClient
 
 from pystache.loader import Loader
 
 from flask import Flask
 app = Flask(__name__)
+
+cache = SimpleCache()
 
 lastfm_key = os.environ.get('lastfm_key')
 lastfm_secret = os.environ.get('lastfm_secret')
@@ -29,26 +33,30 @@ def index():
     loader = Loader()
     template = loader.load_name('index')
 
-    lastfm_client = LastfmClient(lastfm_key, lastfm_secret)
-    recent_tracks = lastfm_client.user.get_recent_tracks('kevbear')['track']
-    recent_track = recent_tracks[0]
-    artist_name = recent_track['artist']['#text']
-    track_name = recent_track['name']
-    mbid = recent_track['mbid']
-    track_info = lastfm_client.track.get_info(
-        artist_name,
-        track_name,
-        mbid,
-    )
-
-    ctx = {
-        'lastfm_result': {
+    lastfm_result = cache.get('lastfm_result')
+    if not lastfm_result:
+        lastfm_client = LastfmClient(lastfm_key, lastfm_secret)
+        recent_tracks = lastfm_client.user.get_recent_tracks('kevbear')['track']
+        recent_track = recent_tracks[0]
+        artist_name = recent_track['artist']['#text']
+        track_name = recent_track['name']
+        mbid = recent_track['mbid']
+        track_info = lastfm_client.track.get_info(
+            artist_name,
+            track_name,
+            mbid,
+        )
+        lastfm_result = {
             'track': track_info['name'],
             'duration': milli_seconds_to_duration(int(track_info['duration'])),
             'album': track_info['album']['title'],
             'artist': track_info['artist']['name'],
             'url': track_info['url'],
         }
+        cache.set('lastfm_result', lastfm_result, timeout=120)
+
+    ctx = {
+        'lastfm_result': lastfm_result,
     }
 
     return pystache.render(template, ctx)

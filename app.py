0119fafde2a476403b9d6441ec449a6254a465cc
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-01-21 00:24:26 +0000

    Moving out lastfm result function and caching both results.

--- app.py
+++ app.py
@@ -9,65 +9,50 @@ from werkzeug.contrib.cache import SimpleCache
 
 from datetime import date
 
-from lastfmclient import LastfmClient
-
 from pystache.loader import Loader
 
 from flask import Flask
 app = Flask(__name__)
 
+from lastfm import get_lastfm_most_recently_listened_details
 from letterboxd import get_letterboxd_most_recently_watched_details
 
 cache = SimpleCache()
 
 loader = Loader()
 
-lastfm_key = os.environ.get('lastfm_key')
-lastfm_secret = os.environ.get('lastfm_secret')
 
-def milli_seconds_to_duration(milli_seconds):
-    total_seconds = milli_seconds / 1000
+def get_cached_result(func, key):
+    cached_result = None
 
-    minutes = total_seconds / 60
-    seconds = total_seconds % 60
+    try:
+        cached_result = cache.get(key)
+        if not cached_result:
+            cached_result = func()
+            cache.set(key, cached_result)
+    except:
+        pass
 
-    return 'PT{}M{}S'.format(minutes, seconds)
+    return cached_result
 
 
 @app.route('/')
 def index():
     template = loader.load_name('index')
 
-    lastfm_result = None
+    lastfm_result = get_cached_result(
+        get_lastfm_most_recently_listened_details,
+        'lastfm_result',
+    )
 
-    try:
-        lastfm_result = cache.get('lastfm_result')
-        if not lastfm_result:
-            lastfm_client = LastfmClient(lastfm_key, lastfm_secret)
-            recent_tracks = lastfm_client.user.get_recent_tracks('kevbear')['track']
-            recent_track = recent_tracks[0]
-            artist_name = recent_track['artist']['#text']
-            track_name = recent_track['name']
-            mbid = recent_track['mbid']
-            track_info = lastfm_client.track.get_info(
-                artist_name,
-                track_name,
-                mbid,
-            )
-            lastfm_result = {
-                'track': track_info['name'],
-                'duration': milli_seconds_to_duration(int(track_info['duration'])),
-                'album': track_info['album']['title'],
-                'artist': track_info['artist']['name'],
-                'url': track_info['url'],
-            }
-            cache.set('lastfm_result', lastfm_result, timeout=120)
-    except:
-        pass
+    letterboxd_result = get_cached_result(
+        get_letterboxd_most_recently_watched_details,
+        'letterboxd_result',
+    )
 
     ctx = {
         'lastfm_result': lastfm_result,
-        'letterboxd_result': get_letterboxd_most_recently_watched_details(),
+        'letterboxd_result': letterboxd_result,
         'year': date.today().year,
     }
 

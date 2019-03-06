660c6a6caebeec5bd0d8a544f9fa4436ad50c70e
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-06 23:32:16 +0000

    Add flask_talisman to wrap Flask app.
    This applies a default Google CSP policy.
    Also removes modernizr due to this causing CSP errors.

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ from datetime import date
 from pystache.loader import Loader
 
 from flask import Flask
+from flask_talisman import Talisman, GOOGLE_CSP_POLICY
 app = Flask(__name__)
+Talisman(app, content_security_policy=GOOGLE_CSP_POLICY)
 
 from lastfm import get_lastfm_most_recently_listened_details
 from letterboxd import get_letterboxd_most_recently_watched_details

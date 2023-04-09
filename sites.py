e53fb496a68ac745e4ce63435d2972302977a87d
Kevin Carrogan <kevin@carrogan.com>
2023-04-09 14:52:08 +0100

    Update sites

--- sites.py
+++ sites.py
@@ -2,9 +2,8 @@ SITES = (
     ("Developer Arguments", "https://www.developerarguments.com"),
     (
         "What colour hoodie is Kevin wearing today?",
-        "https://whatcolourhoodieiskevinwearingtoday.com/",
+        "https://www.whatcolourhoodieiskevinwearingtoday.com/",
     ),
-    ("Manslator", "https://www.manslator.app"),
     (
         "When is Kevin leaving Potato?",
         "https://wheniskevinleavingpotatocom.kevincarrogan.now.sh",
